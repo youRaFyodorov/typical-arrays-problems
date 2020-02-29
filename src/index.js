b378a9d54801a00ba6167f66f48ba2423a937d07
Yury Fyodorov <fedoruch2000@gmail.com>
2020-02-29 13:59:51 +0300

    feat: final version of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (arr === undefined || arr.length === 0){
+    return 0;
+  }
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (arr === undefined || arr.length === 0){
+    return 0;
+  }
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i ++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (arr === undefined || arr.length === 0){
+    return 0;
+  }
+  // let sum = 0;
+  // for (let i = 0; i < arr.length; i ++){
+  //   sum += arr[i];
+  // }
+  // return sum/arr.length;
+  return arr.reduce((a,b) => a + b)/arr.length;
 }
